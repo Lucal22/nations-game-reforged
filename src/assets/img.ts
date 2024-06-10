10aebe8af2ddcc7714fd14e25bdda087928186cd
lucaldev <lucaldev22@gmail.com>
2024-06-10 17:27:32 -0300

    feat: Adding country flags

---
+++ src/assets/img.ts
@@ -0,0 +1,53 @@
+import africaSul from './img/za.svg'
+import alemanha from './img/de.svg'
+import argentina from './img/ar.svg'
+import australia from './img/au.svg'
+import brasil from './img/br.svg'
+import belgica from './img/be.svg'
+import canada from './img/ca.svg'
+import camaroes from './img/cm.svg'
+import grecia from './img/gr.svg'
+import irlanda from './img/ie.svg'
+import coreiaSul from './img/kr.svg'
+import dinamarca from './img/dk.svg'
+import estadosUnidos from './img/us.svg'
+import espanha from './img/es.svg'
+import franca from './img/fr.svg'
+import holanda from './img/nl.svg'
+import reinoUnido from './img/gb.svg'
+import italia from './img/it.svg'
+import japao from './img/jp.svg'
+import mexico from './img/mx.svg'
+import novaZelandia from './img/nz.svg'
+import polonia from './img/pl.svg'
+import portugal from './img/pt.svg'
+import ucrania from './img/ua.svg'
+import uruguai from './img/uy.svg'
+
+export default {
+    africaSul,
+    alemanha,
+    argentina,
+    australia,
+    brasil,
+    belgica,
+    canada,
+    camaroes,
+    grecia,
+    irlanda,
+    coreiaSul,
+    dinamarca,
+    estadosUnidos,
+    espanha,
+    franca,
+    holanda,
+    reinoUnido,
+    italia,
+    japao,
+    mexico,
+    novaZelandia,
+    polonia,
+    portugal,
+    ucrania,
+    uruguai,
+};
